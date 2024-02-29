67d0fbe6ee235409d27205ffa723113cc23d371e
Natanael junior <138621051+njunior93@users.noreply.github.com>
2024-02-29 20:56:26 -0300

    Update Formulario.tsx

--- src/componentes/Formulario/Formulario.tsx
+++ src/componentes/Formulario/Formulario.tsx
@@ -38,7 +38,7 @@ const Formulario = () =>{
       onSubmit={(valores, {setValues}) => 
         { 
           const cadastro = {id: uuidv4(), nome: valores.nome, email: valores.email, data: valores.data, sexo: valores.sexo}
-          fetch('http://localhost:3333/cadastrar',
+          fetch('https://api-njunior93.vercel.app/cadastrar',
           {
             method: 'POST',
             headers: {
