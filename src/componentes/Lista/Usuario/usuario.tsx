c29dd4cfea78456d8c2ea706de9226fa59481ca5
Natanael junior <138621051+njunior93@users.noreply.github.com>
2024-03-19 15:11:39 -0300

    Update usuario.tsx

--- src/componentes/Lista/Usuario/usuario.tsx
+++ src/componentes/Lista/Usuario/usuario.tsx
@@ -18,7 +18,7 @@ const Usuario = ({usuario}:Props) =>{
 
     if(window.confirm(`Deseja excluir o usuario ${usuario.nome}`)){
 
-      fetch(`https://api-njunior93.vercel.app/lista/{usuario.id}`,{
+      fetch(`https://api-njunior93.vercel.app/lista/${usuario.id}`,{
         method: 'DELETE',
         headers: {'Content-Type': 'application/json',},
       })
